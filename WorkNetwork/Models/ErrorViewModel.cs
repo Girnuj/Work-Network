2fb879af251c41d1252e4ad39bd79a6483e7522f
Agustin <agucomba@gmail.com>
2022-05-24 20:05:11 -0300

    Agregar archivos de proyecto.

---
+++ WorkNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
