052e8f82f8110d7ee233bfc97102fe2299d9e7ee
Samuel Montenegro <montenegrosamuel77@outlook.com>
2022-05-27 11:18:34 -0300

    AplicarionDBContext

--- WorkNetwork/Data/ApplicationDbContext.cs
+++ WorkNetwork/Data/ApplicationDbContext.cs
@@ -13,6 +13,15 @@ namespace WorkNetwork.Data
         public DbSet<Pais> Pais { get; set; }
         public DbSet<Provincia> Provincia{ get; set; }
         public DbSet<Localidad> Localidad{ get; set; }
+        public DbSet<Empresa> Empresa{ get; set; }
+        public DbSet<Persona> Persona{ get; set; }
+        public DbSet<PersonaVacante> PersonaVacante{ get; set; }
+        public DbSet<Rubro> Rubro{ get; set; }
+        public DbSet<SubRubro> SubRubro{ get; set; }
+        public DbSet<Usuario> Usuario{ get; set; }
+        public DbSet<Vacante> Vacante{ get; set; }
+        public DbSet<DisponibilidadHoraria> DisponibilidadHoraria{ get; set; }
+
             
     }
 }
