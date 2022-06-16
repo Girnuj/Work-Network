b2ff26db274c99ffc4b2dd51738dc717d77e9ca3
Julian Guastoni <julian_guastoni@hotmail.com>
2022-06-15 21:42:02 -0300

    Creacion del MVC de Provincia

---
+++ WorkNetwork/Controllers/ProvinciasController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkNetwork.Data;
+using WorkNetwork.Models;
+
+namespace WorkNetwork.Controllers
+{
+    public class ProvinciasController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProvinciasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Provincia.ToListAsync());
+        }
+
+        public JsonResult TablaProvincias()
+        {
+            var personas = _context.Provincia.ToList();
+            return Json(personas);
+        }
+        public JsonResult CrearProvincia(int IdProvincia, string NombreProvincia)
+        {
+            bool resultado = false;
+            var provincia = new Provincia
+            {
+                NombreProvincia = NombreProvincia,
+               
+            };
+            _context.Add(provincia);
+            _context.SaveChanges();
+            return Json(resultado);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ WorkNetwork/wwwroot/js/Provincias.js
@@ -0,0 +1,57 @@
+﻿const CompletarTablaProvincias = async () => {
+
+    await VaciarFormulario();
+
+    $.ajax({
+        type: "POST",
+        url: '../../Provincias/TablaProvincias',
+        data: {},
+        success: async (provincias) => {
+            $('#tbody-provincias').empty();
+            $.each(provincias, await function (index, provincia) {
+                console.log(provincia)
+                let claseEliminado = '';
+                $("#tbody-provincias").append(
+                    `<tr class= 'tabla-hover ${claseEliminado}'>
+                        <td class='texto'>${provincia.nombreProvincia}</td>
+                        <td class = 'text-center'>
+                        </td>
+                    </tr>`
+
+
+
+                )
+            });
+
+        },
+        error: (err) => console.log("error en CompletarTablaProvincias", err)
+    });
+
+}
+
+
+const GuardarProvincia = async () => {
+    let idProvincia = $('#idProvincia').val();
+    let nombreProvincia = $('#nombreProvincia').val();
+    let url = '../../Provincias/CrearProvincia';
+    let data = { IdProvincia: idProvincia, NombreProvincia: nombreProvincia };
+
+    //j.query
+    $.post(url, data).done(await function (resultado) {
+        if (resultado == false) {
+            $('#modalCrearProvincia').modal('hide')
+            CompletarTablaProvincias()
+        }
+    }).fail((err) => console.log("error al guardar la Provincia", err))
+}
+
+const AbrirModal = () => {
+    $('#idProvincia').val(0);
+    $('#modalCrearProvincia').modal('show');
+}
+
+const VaciarFormulario = () => {
+
+    $("#idProvincia").val(0);
+    $("#nombreProvincia").val('');
+}
