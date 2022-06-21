39d2d2f11a95d6a5cec013a6ade3fa8b1f462a3e
Agustin <agucomba@gmail.com>
2022-06-21 20:53:24 -0300

    Crear Pais

---
+++ WorkNetwork/Controllers/PaisesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkNetwork.Data;
+using WorkNetwork.Models;
+
+namespace WorkNetwork.Controllers
+{
+    public class PaisesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaisesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Pais.ToList());
+        }
+        public JsonResult TablaPaises()
+        {
+            var paises = _context.Pais;
+            return Json(paises);
+        }
+        public JsonResult CrearPais(string NombrePais)
+        {
+            bool resultado = false;
+            var pais = new Pais
+            {
+                NombrePais = NombrePais,
+            };
+            _context.Add(pais);
+            _context.SaveChanges();
+            return Json(resultado);
+
+        }
+
+    }
+}
---
+++ WorkNetwork/wwwroot/js/Paises.js
@@ -0,0 +1,46 @@
+﻿const CompletarTablaPaises = async () => {
+    await VaciarFormulario()
+    const url = '../../Paises/TablaPaises'
+
+
+    $.get(url).done(
+        paises => {
+            $('#tbody-pais').empty();
+            $.each(paises, async (index, pais) => {
+                console.log('hola')
+                let claseEliminado = '';
+                $('#tbody-pais').append(`<tr class= 'tabla-hover ${claseEliminado}'>
+                        <td class='texto'>pais${pais.nombrePais}</td>
+                        <td class = 'text-center'>
+                        </td>
+                    </tr>`)
+            })
+        }
+    ).fail(e=>console.log(e))
+}
+
+const AbrirModal = () => {
+    $('#idPais').val(0)
+    $('#nombrePais').val('')
+    $('#modalCrearPais').modal('show')
+}
+
+const VaciarFormulario = () => {
+
+    $("#idPais").val(0);
+    $("#nombrePais").val('');
+}
+
+const GuardarPais = async () => {
+    let idPais = $('#idPais').val();
+    let nombrePais = $('#nombrePais').val();
+    let url = '../../Paises/CrearPais';
+    let data = { NombrePais: nombrePais }
+
+    $.post(url, data).done(await function (resultado){
+        if (resultado == false) {
+            $('#modalCrearPais').modal('hide')
+            CompletarTablaPaises()
+        }
+    }).fail(err => console.log('error en crear Pais: ', err ))
+}
