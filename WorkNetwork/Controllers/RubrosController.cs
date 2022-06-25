4e60fe3b91c0bc8abef12b289940ea54b6435813
Agustin <agucomba@gmail.com>
2022-06-25 20:26:54 -0300

    Crear Rubros con validaciones

---
+++ WorkNetwork/Controllers/RubrosController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkNetwork.Data;
+using WorkNetwork.Models;
+
+namespace WorkNetwork.Controllers
+{
+    public class RubrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RubrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Rubro.ToList());
+        }
+        public JsonResult TablaRubros()
+        {
+            var rubros = _context.Rubro.ToList();
+            return Json(rubros);
+        }
+
+        public JsonResult GuardarRubro(int idRubro, string NombreRubro)
+        {
+            int resultado = 0;
+            //Si es 0 es correcto
+            //Si es 1 descripcion vacia
+            //Si es 2 campo existente
+            if (!string.IsNullOrEmpty(NombreRubro))
+            {
+                NombreRubro = NombreRubro.ToUpper();
+                if (idRubro== 0)
+                {
+                    if (_context.Rubro.Any(e => e.NombreRubro== NombreRubro))
+                    {
+                        resultado = 2;
+                    }
+                    else
+                    {
+                        //Aca va el crear
+                        var rubroCrear = new Rubro
+                        {
+                            NombreRubro = NombreRubro
+                        };
+                        _context.Add(rubroCrear);
+                        _context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    if (_context.Rubro.Any(e => e.NombreRubro == NombreRubro && e.RubroID != idRubro))
+                    {
+                        resultado = 2;
+                    }
+                    else
+                    {
+                        //Aca va el editar
+                        var rubro = _context.Rubro.Single(p => p.RubroID == idRubro);
+                        rubro.NombreRubro = NombreRubro;
+                        _context.SaveChanges();
+                    }
+                }
+
+            }
+
+            return Json(resultado);
+        }
+    }
+}
---
+++ WorkNetwork/wwwroot/js/Rubros.js
@@ -0,0 +1,62 @@
+﻿const CompletarTablaRubro = async() => {
+    await VaciarFormulario()
+    let url = '../../Rubros/TablaRubros'
+
+
+    $.get(url).done(async rubros => {
+        $('#tbody-rubros').empty();
+        $.each(rubros, await function (i, rubro) {
+
+            let claseEliminado = '';
+            let botones = `<btn type='button' class= 'btn btn-outline-success btn-sm me-3' onclick = "BuscarRubro(${rubro.rubroID})"><i class="bi bi-pencil-square"></i> Editar</btn>
+                                <btn type='button' class = 'btn btn-outline-danger btn-sm'onclick = "EliminarRubro(${rubro.rubroID},1)"><i class="bi bi-trash3"></i> Eliminar</btn>`
+
+            if (rubro.eliminado) {
+                claseEliminado = 'table-danger';
+                botones = `<btn type='button' class = 'btn btn-outline-warning btn-sm'onclick = "EliminarRubro(${rubro.rubroID},0)"><i class="bi bi-recycle"></i> Activar</btn>`
+            }
+            $('#tbody-rubros').append(
+                `<tr class= 'tabla-hover ${claseEliminado}'>
+                        <td class='texto'>${rubro.nombreRubro}</td>
+                        <td class = 'text-center'>
+                            ${botones}
+                        </td>
+                    </tr>`
+            )
+        })
+    })
+}
+
+const GuardarRubro = () => {
+    let idRubro = $('#idRubro').val();
+    let nombreRubro = $('#nombreRubro').val().trim();
+    let alertRubro = $('#alertRubro')
+    let url = '../../Rubros/GuardarRubro'
+    let data = { idRubro: idRubro, NombreRubro: nombreRubro }
+    if (nombreRubro != '' && nombreRubro != null) {
+        $.post(url, data).done(resultado => {
+            if (resultado == 0) {
+                $('#modalCrearRubro').modal('hide')
+                CompletarTablaRubro();
+            }
+            if (resultado == 2) {
+                alertRubro.removeClass('visually-hidden').text('El rubro ingresado ya existe')
+            }
+        }).fail(e => console.log('error en guardar rubro' + e))
+    } else {
+        alertRubro.removeClass('visually-hidden')
+    }
+
+}
+
+const AbrirModal = ()=> {
+    $('#titulo-modal-rubro').text('Nuevo Rubro')
+    $('#idRubro').val(0);
+    $('#modalCrearRubro').modal('show');
+    $('#alertRubro').addClass('visually-hidden')
+}
+const VaciarFormulario = ()=> {
+    $("#idRubro").val(0);
+    $('#nombreRubro').val('');
+    $('#alertRubro').addClass('visually-hidden')
+}
