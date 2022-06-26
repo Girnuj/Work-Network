56334d20e7565f567a113ac3fcf8991c2ee2df3a
Agustin <agucomba@gmail.com>
2022-06-26 20:40:08 -0300

    Empresas

---
+++ WorkNetwork/Controllers/EmpresasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkNetwork.Data;
+
+namespace WorkNetwork.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmpresasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult TablaEmpresas()
+        {
+            var empresas = _context.Empresa.ToList();
+            return Json(empresas);
+        }
+    }
+}
