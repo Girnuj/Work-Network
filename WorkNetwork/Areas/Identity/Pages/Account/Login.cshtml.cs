7cb710bea61f3845a5e79fed86a0365f91d8c30f
Agu Comba <agucomba@gmail.com>
2022-08-24 11:49:40 -0300

    renderizado condicioneal de Layout de ingreso

--- WorkNetwork/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WorkNetwork/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -107,8 +107,6 @@ namespace WorkNetwork.Areas.Identity.Pages.Account
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
-            if (ModelState.IsValid)
-            {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
@@ -131,7 +129,6 @@ namespace WorkNetwork.Areas.Identity.Pages.Account
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }
-            }
 
             // If we got this far, something failed, redisplay form
             return Page();
