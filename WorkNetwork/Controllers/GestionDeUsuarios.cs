32d05c777d8cfa5248f6ace886f52fe25a5c28c7
Agustin <agucomba@gmail.com>
2022-08-29 21:36:04 -0300

    Creacion de login y register JS

--- WorkNetwork/Controllers/GestionDeUsuarios.cs
+++ WorkNetwork/Controllers/GestionDeUsuarios.cs
@@ -10,15 +10,67 @@ namespace WorkNetwork.Controllers
         private readonly SignInManager<IdentityUser> _singInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-
-        public GestionDeUsuarios(ApplicationDbContext context, SignInManager<IdentityUser> singInManager, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        //Para la creacion del usuario
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        public GestionDeUsuarios(ApplicationDbContext context, SignInManager<IdentityUser> singInManager, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager,IUserStore<IdentityUser> userStore)
         {
             _context = context;
             _singInManager = singInManager;
             _userManager = userManager;
             _roleManager = roleManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
         }
 
+        public async Task<JsonResult> Ingresar(string email, string password)
+        {
+            bool logueado = false;
+
+            var result = await _singInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                logueado = true;
+            }
+
+            return Json(logueado);
+        }
+        public async Task<JsonResult> Registrar(string email, string password) {
+            bool registrado=false;
+            var user = CreateUser();
+            await _userStore.SetUserNameAsync(user, email, CancellationToken.None);
+            await _emailStore.SetEmailAsync(user, email, CancellationToken.None);
+            var result = await _userManager.CreateAsync(user, password);
+            if (result.Succeeded)
+            {
+                var userId = await _userManager.GetUserIdAsync(user);
+                await _singInManager.SignInAsync(user, isPersistent: false);
+                registrado = true;
+            }
+
+            return Json(registrado);
+        }
+private IdentityUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<IdentityUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
+                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+        private IUserEmailStore<IdentityUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<IdentityUser>)_userStore;
+        }
 
         public IActionResult Index()
         {
---
+++ WorkNetwork/wwwroot/js/GestionDeUsuarios.js
@@ -0,0 +1,20 @@
+﻿function loginEmpresa(){	
+			event.preventDefault();
+			let correoEmpresa =	$('#correoEmpresa').val();
+			let passEmpresa = $('#passwordEmpresa').val();
+			let url = '../../GestionDeUsuarios/Ingresar';
+			let data = { email: correoEmpresa, password:passEmpresa };
+	$.post(url, data).done(resultado => {
+		resultado ? console.log('logeado') : alert('usuario o contraseña incorrecta')
+			}).fail(e => console.log(e))
+		}
+function register() {
+	event.preventDefault();
+	let correo = $('#Email').val();
+	let pass = $('#Password').val();
+	let passConfirm = $('#ConfirmPassword').val();
+	let url = '../../GestionDeUsuarios/Registrar';
+	let data = { email: correo, password: pass }
+	$.post(url, data).done(resultado => resultado ? console.log('logeado') : alert('usuario o contraseña incorrecta')).fail(e=>console.log(e))
+	
+}
